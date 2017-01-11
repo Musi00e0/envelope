7a34d5ec985092292c9652d07ba154044edc22d5
Musi00e0 <leo.sieben.iii@gmail.com>
2017-01-10 23:54:19 -0600

    - set working directory for running / debugging - fixed output directory pathing - example now reads the data file and outputs to console

--- envelope/dev/src/_driver.cpp
+++ envelope/dev/src/_driver.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
 
 int main(int argc, char** argv)
 {
+	ifstream dataFile("data/2017-01.csv");
+	if (dataFile.is_open())
+	{
+		string line;
+		while (!dataFile.eof())
+		{
+			getline(dataFile, line);
+			cout << line << endl;
+		}
+	}
+	else
+	{
+		cout << "Couldn't open the data file.  Check working directory" << endl;
+	}
+
 	return 0;
 }
